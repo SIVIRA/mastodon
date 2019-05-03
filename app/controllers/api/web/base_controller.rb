f129711360362c9f1169c4adc9f9523337a83d4c
Toshihiko Ryugo <ryugo@sivira.co>
2019-05-03 18:27:03 +0900

    ログイン時のCSRF対策を無効化

--- app/controllers/api/web/base_controller.rb
+++ app/controllers/api/web/base_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Api::Web::BaseController < Api::BaseController
-  protect_from_forgery with: :exception
-
+#  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   rescue_from ActionController::InvalidAuthenticityToken do
     render json: { error: "Can't verify CSRF token authenticity." }, status: 422
   end
